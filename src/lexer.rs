32a0f956261a599c19e60da4aa8aa4bdb5c6ae2b
ledwastaken <ledwastaken@proton.me>
2026-02-02 23:48:47 +0100

    feat: add lexer.rs

---
+++ src/lexer.rs
@@ -0,0 +1,17 @@
+pub struct Lexer {
+    // FIXME
+}
+
+impl Lexer {
+    pub fn new() -> Self {
+        Lexer {}
+    }
+
+    pub fn peek(&mut self) {
+        // FIXME
+    }
+
+    pub fn pop(&mut self) {
+        // FIXME
+    }
+}
