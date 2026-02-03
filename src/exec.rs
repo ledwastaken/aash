2a8a4af3cb5524f368866a2a4800bbdc558431a8
ledwastaken <ledwastaken@proton.me>
2026-02-03 20:57:08 +0100

    feat: add exec.rs

---
+++ src/exec.rs
@@ -0,0 +1,5 @@
+use crate::ast::SimpleCommand;
+
+pub fn execute_simple_command(simple_command: &SimpleCommand) {
+    // FIXME
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod ast;
+mod exec;
 mod lexer;
 mod parser;
 
 use ast::Ast;
+use exec::execute_simple_command;
 use lexer::Lexer;
 use parser::parse_input;
 
@@ -20,7 +22,7 @@ pub fn parse_execute_loop() {
     loop {
         match ast {
             Ast::SimpleCommand(ref simple_command) => {
-                // FIXME
+                execute_simple_command(simple_command);
             }
             Ast::Eof => break,
         }
