54ea96a216c561e91c6c74934d393eea323f736a
ledwastaken <ledwastaken@proton.me>
2026-02-04 12:21:06 +0100

    feat: add input handling

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,34 @@ mod exec;
 mod lexer;
 mod parser;
 
+use std::fs::File;
+use std::io::{Cursor, Read};
+
 use ast::Ast;
 use exec::execute_simple_command;
 use lexer::Lexer;
 use parser::parse_input;
 
 fn main() {
-    // FIXME
+    let mut args = std::env::args();
+    let program_name = args.next().unwrap();
 
-    parse_execute_loop();
+    match args.next() {
+        Some(arg) => match arg.as_str() {
+            "-c" => match args.next() {
+                Some(src) => parse_execute_loop(&mut Cursor::new(src)),
+                None => {
+                    eprintln!("{}: -c: option requires an argument", program_name);
+                    std::process::exit(2);
+                }
+            },
+            _ => parse_execute_loop(&mut File::open(arg).unwrap()),
+        },
+        None => parse_execute_loop(&mut std::io::stdin()),
+    }
 }
 
-pub fn parse_execute_loop() {
+pub fn parse_execute_loop<R: Read>(src: &mut R) {
     let mut lexer = Lexer::new();
 
     let ast = parse_input(lexer);
