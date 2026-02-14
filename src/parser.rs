ea01d09a484aa60f63ecaa630bda010475f8c9f3
ledwastaken <ledwastaken@proton.me>
2026-02-14 10:17:46 +0100

    feat(parser): add input rule parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,22 @@
-use crate::ast::Ast;
-use crate::lexer::Lexer;
+use std::io::Read;
 
-// pub fn parse_input<R: Read>(lexer: &mut Lexer<R>) -> Ast {
-//     Ast::Eof // FIXME
-// }
+use crate::ast::Ast;
+use crate::lexer::{Lexer, Token};
+
+pub fn parse_input<R: Read>(lexer: &mut Lexer<R>) -> Option<Ast> {
+    match lexer.peek() {
+        Token::Eof => {
+            lexer.pop();
+            None
+        }
+        Token::Newline => {
+            lexer.pop();
+            Some(Ast::None)
+        }
+        _ => {
+            // TODO parse_list
+            lexer.pop();
+            Some(Ast::None)
+        }
+    }
+}
