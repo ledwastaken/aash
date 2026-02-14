2572ea0ae29c93789d1695179a17c8084bf583cd
ledwastaken <ledwastaken@proton.me>
2026-02-14 10:16:30 +0100

    feat(ast): add Debug derivation

--- src/ast.rs
+++ src/ast.rs
@@ -2,6 +2,7 @@ pub mod simple_command;
 
 pub use self::simple_command::SimpleCommand;
 
+#[derive(Debug)]
 pub enum Ast {
     SimpleCommand(SimpleCommand),
     None,
--- src/ast/simple_command.rs
+++ src/ast/simple_command.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct SimpleCommand {
     // FIXME
 }
