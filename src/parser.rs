668284ade1ee4d2a0df0f06f5beca4098fdea513
ledwastaken <ledwastaken@proton.me>
2026-02-15 13:10:20 +0100

    feat(parser): add ParseResult enum

--- src/parser.rs
+++ src/parser.rs
@@ -3,14 +3,24 @@ use std::io::Read;
 use crate::ast::Ast;
 use crate::lexer::{Lexer, Token};
 
+enum ParseResult {
+    Success(Ast),
+    UnexpectedToken(Token),
+}
+
 pub fn parse_input<R: Read>(lexer: &mut Lexer<R>) -> Option<Ast> {
     let token = lexer.next();
-
-    println!("{:?}", token);
 
     match token {
         Token::Eof => None,
         Token::Newline => Some(Ast::None),
-        _ => Some(Ast::None), // TODO parse_list
+        token => match parse_list(lexer, token) {
+            ParseResult::Success(ast) => Some(ast),
+            ParseResult::UnexpectedToken(_) => Some(Ast::None) // TODO handle parse error
+        },
     }
+}
+
+fn parse_list<R: Read>(lexer: &mut Lexer<R>, token: Token) -> ParseResult {
+    ParseResult::Success(Ast::None) // TODO
 }
