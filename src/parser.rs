db44191b18dc5108ba221228ae8ebbc4fe6ad8d0
ledwastaken <ledwastaken@proton.me>
2026-02-16 23:20:09 +0100

    feat(parser): add command lists parsing

--- src/parser.rs
+++ src/parser.rs
@@ -30,7 +30,26 @@ pub fn parse_input<R: Read>(lexer: &mut Lexer<R>) -> Option<Ast> {
 }
 
 fn parse_list<R: Read>(lexer: &mut Lexer<R>, token: Token) -> ParseResult {
-    parse_and_or(lexer, token)
+    match parse_and_or(lexer, token) {
+        ParseResult::Success(ast, Token::Semicolon) => {
+            let mut commands = vec![ast];
+
+            loop {
+                let next_token = lexer.next();
+                match parse_and_or(lexer, next_token) {
+                    ParseResult::Success(next_ast, Token::Semicolon) => commands.push(next_ast),
+                    ParseResult::Success(next_ast, last_token) => {
+                        commands.push(next_ast);
+                        return ParseResult::Success(Ast::List(commands), last_token);
+                    }
+                    ParseResult::UnexpectedToken(unexpected_token) => {
+                        return ParseResult::Success(Ast::List(commands), unexpected_token);
+                    }
+                }
+            }
+        }
+        e => e,
+    }
 }
 
 fn parse_and_or<R: Read>(lexer: &mut Lexer<R>, token: Token) -> ParseResult {
