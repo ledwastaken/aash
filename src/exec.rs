9e9e3d4144ff7608b3ed71d9c79358dd08c16f30
ledwastaken <ledwastaken@proton.me>
2026-02-16 23:05:18 +0100

    feat(exec): feat list of ast execution

--- src/exec.rs
+++ src/exec.rs
@@ -5,6 +5,11 @@ use crate::ast::Ast;
 pub fn execute_ast(ast: Ast) {
     match ast {
         Ast::SimpleCommand { program, args } => execute_simple_command(program, args),
+        Ast::List(commands) => {
+            for cmd in commands {
+                execute_ast(cmd);
+            }
+        }
         Ast::None => (),
     }
 }
