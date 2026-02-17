61f92c3545a458896b68a1aeceb345aa059b7446
ledwastaken <ledwastaken@proton.me>
2026-02-17 21:47:27 +0100

    feat(ast): add ast for if commands

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,14 @@
 #[derive(Debug)]
 pub enum Ast {
-    SimpleCommand { program: String, args: Vec<String> },
+    SimpleCommand {
+        program: String,
+        args: Vec<String>,
+    },
     List(Vec<Ast>),
+    IfCommand {
+        condition: Box<Ast>,
+        then_branch: Box<Ast>,
+        else_branch: Option<Box<Ast>>,
+    },
     None,
 }
